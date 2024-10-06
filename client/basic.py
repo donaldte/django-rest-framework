485f5b0afd9fdbb9bddb92b1b9e69fd03b49bacd
donaldte <donaldtedom0@gmail.com>
2024-10-06 17:44:12 +0100

    feat: create your first api

--- client/basic.py
+++ client/basic.py
@@ -6,12 +6,11 @@ endpoint: c'est l'adresse du serveur (http://httpbin.org)
 en francais endpoint signifie point de terminaison
 """
 
-endpoint = "http://localhost:8000"
+endpoint = "http://localhost:8000/api/v1/"
 
 # on fait une requete GET sur l'endpoint
-response = requests.get(endpoint, data={"name": "John", "age": 30})
+response = requests.get(endpoint)
 
-print(response.text)
+print(response.json())
 print(response.status_code)
 
-{'args': {}, 'data': '', 'files': {}, 'form': {}, 'headers': {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate', 'Host': 'httpbin.org', 'User-Agent': 'python-requests/2.32.3', 'X-Amzn-Trace-Id': 'Root=1-67027e1b-1e41fb814d9f288614865cb7'}, 'json': None, 'method': 'GET', 'origin': '129.0.80.240', 'url': 'http://httpbin.org/anything'}
